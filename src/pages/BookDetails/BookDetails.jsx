b074580c59ceb3fddbe49e6391137c176a68e769
mtmsujan <mdraju3248@gmail.com>
2025-04-30 02:23:20 +0600

    Added a sweetaleart popup

--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useLoaderData, useParams } from 'react-router'
 import { setItemsData } from '../../utils/AddToData';
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+const MySwal = withReactContent(Swal);
 
 const BookDetails = () => {
     const {id} = useParams();
@@ -21,6 +24,11 @@ const BookDetails = () => {
 
 
     const handleBookStorage = (id) => {
+      Swal.fire({
+        title: "Good job!",
+        text: "You clicked the button!",
+        icon: "success",
+      });
       setItemsData(id);
     }
 
